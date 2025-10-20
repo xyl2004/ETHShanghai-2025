ea428efa886895323ab0d0121625d6f233bd4dcc
sapphire feng <sapphire@sapphiredeMacBook-Air.local>
2025-10-20 12:23:11 +0800

    fix: delete channels

--- projects/Timelocklabs-Timelock/front-end/app/notify/page.tsx
+++ projects/Timelocklabs-Timelock/front-end/app/notify/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import Channel from './components/channel';
 import Email from './components/email/index';
 import EmailRulesHeader from './components/email/components/EmailRulesHeader';
 
@@ -8,7 +7,6 @@ const EmailNotificationPage: React.FC = () => {
 	return (
 		<div className='flex flex-col space-y-8'>
 			<EmailRulesHeader />
-			<Channel />
 			<Email />
 		</div>
 	);
