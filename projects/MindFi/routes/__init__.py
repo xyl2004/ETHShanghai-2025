210abbb45922f4e0649be6f509cacd0b0b7ceca8
Tools-touch <zj2454547329@qq.com>
2025-10-20 18:18:09 +0800

    commit

--- projects/MindFi/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-from routes.user_route import user_blueprint
-
-api_blueprint = Blueprint('api', __name__, url_prefix='/api')
-
-api_blueprint.register_blueprint(user_blueprint)
